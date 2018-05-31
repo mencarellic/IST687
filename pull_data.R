ee295bfd5f651282ac9cba08ee2246ac593e9e3d
Carlo <mencarelli.c@gmail.com>
2018-05-31 17:11:48 -0400

    Cleaning up code. Adding GC.

--- pull_data.R
+++ pull_data.R
@@ -1,32 +1,37 @@
 ## Require some base packages
 require(RCurl)
 
-## Gather data from Kaggle
+## Set some base variables
 loginURL <- "https://www.kaggle.com/account/login"
 dataURL  <- "https://www.kaggle.com/aaronschlegel/austin-animal-center-shelter-outcomes-and/downloads/aac_shelter_cat_outcome_eng.csv"
-
 tempDir = tempdir()
 tempFile = tempfile(tmpdir=tempDir, fileext=".zip")
+agent <- "Mozilla/5.0"
 
 ## Call file that contains the following:
 ## creds <- list(UserName="USERNAME",
 ##               Password="PASSWORD")
 source("credentials.R")
 
-agent <- "Mozilla/5.0"
+## Curl options and the logging into Kaggle
 curl = getCurlHandle()
-curlSetOpt(cookiejar="cookies.txt",  useragent = agent, followlocation = TRUE, curl=curl)
-welcome=postForm(loginURL, .params = creds, curl=curl)
+curlSetOpt(cookiejar="cookies.txt", useragent=agent, followlocation=TRUE, curl=curl)
+welcome=postForm(loginURL, .params= creds, curl=curl)
 
-bdown=function(url, file, curl){
-  f = CFILE(file, mode="wb")
-  curlPerform(url = url, writedata = f@ref, noprogress=FALSE, curl = curl)
-  close(f)
-}
 
-ret = bdown(dataURL, tempFile,curl)
+## Download file
+f = CFILE(tempFile, mode="wb")
+curlPerform(url=dataURL, writedata=f@ref, noprogress=FALSE, curl=curl)
+close(f)
 
-unzip(tempFile,exdir=tempDir)
-name <- unzip(tempFile,list=TRUE)
-filename <- paste(tempDir,"\\",name$Name,sep="")
+## Unzip file and load into df
+unzip(tempFile, exdir=tempDir)
+name <- unzip(tempFile, list=TRUE)
+filename <- paste(tempDir, "\\", name$Name, sep="")
 data <- read.csv(filename)
+
+
+##Cleanup and GC
+remove(creds,curl,name,ret,agent,dataURL,f,filename,loginURL,tempDir,tempFile,welcome)
+GC()
+

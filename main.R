f7d313d0733d3451040756dd12185510c4365a56
Carlo <mencarelli.c@gmail.com>
2018-06-18 22:21:19 -0400

    Cleaning up. Updated some stat gathering.

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 ## Load libraries, install if they aren't there
-packageList <- c("ggplot2","neuralnet")
+packageList <- c("ggplot2","neuralnet","e1071")
 packageNew <- packageList[!(packageList %in% installed.packages()[,"Package"])]
 if(length(packageNew)) install.packages(packageNew)
 lapply(packageList, library, character.only = TRUE)
@@ -31,53 +31,72 @@ data$name <- gsub("\\*","",data$name)
 
 
 #### Transforming some nominal variables into binary
-data$adoptBinary <- ifelse(tolower(data$outcome_type) %in% c("adoption","transfer","return to owner"),1,0)
-
+data$adoptBinary <- ifelse(tolower(data$outcome_type) %in% c("adoption"),1,0)
 data$cfaBinary <- ifelse(data$cfa_breed==TRUE,1,0)
 data$domesticBinary <- ifelse(data$domestic_breed==TRUE,1,0)
 
-#### Drop NA adoptionBinary
+#### Drop NA adoptionBinary and outcome_type
+data <- data[!is.na(data$outcome_type),]
 data <- data[!is.na(data$adoptBinary),]
-
-### Splitting coat_pattern into binary variables
-newdata <- cbind(data[1:20], sapply(levels(data$coat_pattern),function(x) as.integer(x==data$coat_pattern)), data[22:24])
-
-
-
-### Basic Regression
 
 
 ##### Stats
 ## Total rows
 totalRows <- nrow(data)
 
-## Avg Dropoff Age
-### Possible?
 
 ## CFA a factor in adoption?
 cfa.Total <- length(which(data$cfa_breed==TRUE)) ## 1743
 cfa.Adopted <- length(which(data$cfa_breed==TRUE&data$adoptBinary==1)) ## 823
 cfa.NotAdopted <- length(which(data$cfa_breed==TRUE&data$adoptBinary==0)) ## 920
+nonCFA.Total <- length(which(data$cfa_breed==FALSE)) ## 27675
 nonCFA.Adopted <- length(which(data$cfa_breed==FALSE&data$adoptBinary==1)) ## 11909
 nonCFA.NotAdopted <- length(which(data$cfa_breed==FALSE&data$adoptBinary==0)) ## 15766
-
+adoptionRate.CFA <- cfa.Adopted / cfa.Total ## 0.4721744
+adoptionRate.NonCFA <- nonCFA.Adopted / nonCFA.Total ## 0.4303162
+adoptionRate.df <- data.frame(c(adoptionRate.CFA,adoptionRate.NonCFA))
+rownames(adoptionRate.df) <- c("CFA Adoption", "Non CFA Adoption")
+colnames(adoptionRate.df) <- "Rate"
+## Bar plot
+ggplot(adoptionRate.df, aes(x=rownames(adoptionRate.df), y=Rate)) +
+  geom_bar(stat="identity") +
+  scale_y_continuous(limits= c(0,1)) +
+  labs(x="Cat Classification", y="Rate of Adoption") +
+  ggtitle("Adoption Rates: CFA vs Non-CFA") +
+  theme(plot.title = element_text(hjust = 0.5))
 
 ## Adoptions over time (by year)
 aggDate <- aggregate(data$adoptBinary, by=list(data$outcome_year),sum)
 ## Remove 2013 and 2018 (incomplete years of data)
 aggDate <- aggDate[-which(aggDate$Group.1==c(2013,2018)), ]
 ## Plotting
-ggplot(data=aggDate,aes(y=x,x=Group.1)) + geom_point() + geom_line()
+ggplot(data=aggDate,aes(y=x,x=Group.1)) +
+  geom_point() +
+  geom_line() +
+  labs(x="Year", y="Adoptions") +
+  ggtitle("Adoptions Over Time") +
+  theme(plot.title = element_text(hjust=0.5))
 
 ## Avg Adoption Age
+data$trueAge <- difftime(as.Date(data$datetime), data$date_of_birth, units="days")
+mean(data$trueAge)
 mean(data$outcome_age_.days.) ## 509.4463 Days
+aggOutcomesByAge <- aggregate(data$trueAge, by=list(data$outcome_type), mean)
+ggplot(data=aggOutcomesByAge, aes(y=x, x=Group.1)) +
+  geom_bar(stat="identity") +
+  scale_y_continuous() +
+  labs(x="Outcome Type", y="Age at Outcome") +
+  ggtitle("Average Age by Outcome Type (Days)") +
+  theme(plot.title=element_text(hjust=0.5), axis.text.x=element_text(angle=45, hjust=1))
+  
 
 ## Spay/Neuter
 maleFixedCount <- length(which(data$Spay.Neuter == TRUE & data$sex == 'Male'))
 femaleFixedCount <- length(which(data$Spay.Neuter == TRUE & data$sex == 'Female'))
 maleFixed <- data[which(data$Spay.Neuter == TRUE & data$sex == 'Male'),]
 femaleFixed <- data[which(data$Spay.Neuter == TRUE & data$sex == 'Female'),]
-fixedStats <- with(data, table(sex,Spay.Neuter))
+plot(fixedStats, main="Proportion of Spaying & Neutering", xlab="Sex", ylab="Spayed / Neutered?")
+
 
 ## Adoption Days
 outcomeDays <- as.data.frame(table(data$outcome_weekday))
@@ -99,15 +118,28 @@ mean(round(difftime(data$datetime,data$date_of_birth,units="days"))) ## 534.4272
 aggNames <- aggregate(data.frame(count=data$name), list(value=data$name),length)
 aggNames.Top10 <- head(aggNames[order(aggNames$count,decreasing=TRUE),],n=10)
 
-length(which(data$name=="Charlie" & data$sex=="Male"))/nrow(data)
-length(which(data$name=="Charlie" & data$sex=="Female"))/nrow(data)
-
-length(which(data$name %in% aggNames.Top10$value & data$sex=="Male"))/nrow(data)
 
 ## Number of outcomes per day
 aggOutcomePerDay <- aggregate(data.frame(count=data$datetime), list(value=as.Date(data$datetime)), length)
 aggAdoptionPerDay <- aggregate(data.frame(count=tolower(data$outcome_type)=="adoption"), list(value=as.Date(data$datetime),outcomeType=data$outcome_type), length)
+ggplot(data=aggAdoptionPerDay, aes(x=aggAdoptionPerDay$value,y=aggAdoptionPerDay$count)) +
+  geom_point() + 
+  labs(x="Year", y="Adoptions") +
+  ggtitle("Adoptions Per Day") +
+  theme(plot.title = element_text(hjust=0.5))
 plot(x=aggAdoptionPerDay$value,y=aggAdoptionPerDay$count)
 
-formula <- as.formula(adoptBinary~ outcome_month + Spay.Neuter + trueAge + outcome_hour + factor(outcome_weekday))
-summary(lm(formula,data=data))
+
+## Basic linear model
+basicFormula <- as.formula(adoptBinary~ Spay.Neuter + trueAge + outcome_month + outcome_hour)
+summary(lm(basicFormula,data=data))
+
+
+## Logistic Regression
+g <- glm(basicFormula,data=data,family='binomial')
+summary(glm(basicFormula,data=data,family='binomial'))
+curve(predict(g,data.frame(trueAge=x), type="resp"), add=TRUE)
+
+
+## SVM
+summary(svm(basicFormula,data=data))
